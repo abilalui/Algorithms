74b5af7d9a85edc0f5dc7a6bedb2ac3f7777d570
abilalui <ahmadbilal-hassan@uiowa.edu>
2023-05-11 11:52:35 -0500

    detele method and its helper method transplant were added. readMe.md file for Binary Search Tree was updated as well.

--- src/Trees/Binary Search Tree/BinarySearchTree.java
+++ src/Trees/Binary Search Tree/BinarySearchTree.java
@@ -64,6 +64,66 @@ public class BinarySearchTree {
         }
     }
 
+    /**
+     * findMinimum - method to find minimum value node
+     * @param node - take node as root of the tree and find minimum value of the tree.
+     * @return minimum value of the tree
+     */
+    public Node findMinimum( Node node ) {
+        if ( node == null || node.getLeft() == null ) {
+            return node;
+        }
+        return findMinimum( node.getLeft() );
+    }
+
+    /**
+     * findMaximum - method to find the maximum value of node
+     * @param node - take node as root of the tree
+     * @return minimum value of the tree
+     */
+    public Node findMaximum( Node node ) {
+        if ( node == null || node.getRight() == null ) {
+            return node;
+        }
+        return findMaximum( node.getRight() );
+    }
+
+    /**
+     * findSuccessor - method for finding successor of the tree
+     */
+    public Node findSuccessor( Node node ) {
+
+        if ( node.getRight() != null ) {
+            return findMinimum( node.getRight() );
+        }
+        Node parentNode = node.getParent();
+
+        while ( parentNode != null && node == parentNode.getRight() ) { // check eqality
+            node = parentNode;
+            parentNode = parentNode.getParent();
+        }
+        return parentNode;
+    }
+
+    /**
+     * findPredecessor - method that find the predecessor of given node in the tree
+     * @param node
+     * @return predecessor of node in the tree
+     */
+    public Node findPredecessor( Node node ) {
+
+        if ( node.getLeft() != null ) {
+            return findMaximum( node.getLeft() );
+        }
+        Node parentNode = node.getParent();
+
+        while( parentNode != null && node == parentNode.getLeft() ) {
+            node = parentNode;
+            parentNode = parentNode.getParent();
+        }
+        return parentNode;
+    }
+
 
     /**
      * insert - insert a new data inside the given tree
@@ -84,20 +144,66 @@ public class BinarySearchTree {
                 childNode = childNode.getRight();
             }
         }
-
         if ( root != null ) {
             if (data < newNode.getData()) {
-                newNode.setLeft(new Node(data));
+                newNode.setLeft(new Node(data, newNode));
             } else {
-                newNode.setRight(new Node(data));
+                newNode.setRight(new Node(data, newNode));
             }
         } else {
-            root = new Node( data );
+            root = new Node( data, null );
         }
 
     }
 
 
+    /**
+     * This method is a helper for delete node. It replaces one subtree as
+     * a child of its parent with another subtree.
+     * @param n - node n to be replaced by node m
+     * @param m - node m to be replaced by node n
+     */
+    public void transplant( Node n, Node m ) {
+
+        if ( n.getParent() == null ) {
+            setRoot( m );
+        } else if ( n.getParent().getLeft() == n ) {
+            n.getParent().setLeft( m );
+        } else {
+            n.getParent().setRight( m );
+        }
+
+        if ( m != null ) {
+            m.setParent( n.getParent() );
+        }
+    }
+
+    /**
+     * delete - this method deletes a given node from the tree
+     * @param node - node to be deleted from the tree
+     */
+    public void delete( Node node ) {
+
+        if ( node.getLeft() == null ) {
+            this.transplant( node, node.getRight() );
+        } else if ( node.getRight() == null ) {
+            this.transplant( node, node.getLeft() );
+        } else {
+            Node nodeSuccessor = findSuccessor( node );
+
+            if ( nodeSuccessor.getParent() != node ) {
+
+                this.transplant( nodeSuccessor, nodeSuccessor.getRight());
+                nodeSuccessor.setRight( node.getRight() );
+                nodeSuccessor.getRight().setParent( nodeSuccessor );
+
+            }
+
+            this.transplant( node, nodeSuccessor );
+            nodeSuccessor.setLeft( node.getLeft() );
+            nodeSuccessor.getLeft().setParent( nodeSuccessor );
+        }
+    }
 
 
     public static void main( String[] args ) {
@@ -105,20 +211,20 @@ public class BinarySearchTree {
         //Node node = new Node(1);
 
         BinarySearchTree BSTree = new BinarySearchTree( null );
-        BSTree.root = new Node(1);
-        BSTree.root.setLeft( new Node(2) );
-        BSTree.root.setRight( new Node(3));
-        BSTree.root.getLeft().setLeft( new Node(4) );
-        BSTree.root.getLeft().setRight( new Node ( 5));
+        BSTree.root = new Node(5, null);
+        BSTree.root.setLeft( new Node(3, BSTree.root ) );
+        BSTree.root.setRight( new Node(10, BSTree.root ));
+        BSTree.root.getLeft().setLeft( new Node(2, BSTree.root.getLeft() ) );
+        BSTree.root.getLeft().setRight( new Node ( 4, BSTree.root.getLeft()));
+
+        //BSTree.insert( 8 );
+
+        System.out.println( "The predecessor of 5 is : "
+                            + BSTree.findPredecessor( BSTree.root ).getData() );
 
         BSTree.inOrderTraverse( BSTree.root );
         System.out.println("================================================");
 
-        BSTree.preOrderTraverse( BSTree.root );
-        System.out.println("================================================");
-
-        BSTree.postOrderTraverse( BSTree.root );
-        System.out.println("================================================");
 
     }
 }
